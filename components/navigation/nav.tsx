baa2081c413f6513cd42f13601e4d358f126019c
Antonov-S <anto1nov1@gmail.com>
2024-07-06 01:47:28 +0300

    Creating Our Navbar

---
+++ components/navigation/nav.tsx
@@ -0,0 +1,21 @@
+import { auth } from "@/server/auth";
+
+import { UserButton } from "./user-button";
+
+export default async function Nav() {
+  const session = await auth();
+
+  if (session)
+    return (
+      <header className="bg-slate-500 py-4">
+        <nav>
+          <ul className="flex justify-between">
+            <li>Logo</li>
+            <li>
+              <UserButton user={session?.user} expires={session?.expires} />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+}
