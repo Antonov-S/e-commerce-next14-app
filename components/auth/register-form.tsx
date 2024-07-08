da143837c06a7eff0f9005e8d4ef22be31f80d3f
Antonov-S <anto1nov1@gmail.com>
2024-07-08 22:48:28 +0300

    Creating Register Page

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAction } from "next-safe-action/hooks";
+
+import { AuthCard } from "@/components/auth/auth-card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { RegisterSchema } from "@/types/register-schema";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export const RegisterForm = () => {
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      name: ""
+    }
+  });
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    // execute(values);
+  };
+
+  return (
+    <AuthCard
+      cardTitle="Create an account 🎉"
+      backButtonHref="/auth/login"
+      backButtonLabel="Already have an account?"
+      showSocials
+    >
+      <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="S_Antonov" type="text" />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="anto1nov1@gmail.com"
+                        type="email"
+                        autoComplete="email"
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="*******"
+                        type="password"
+                        autoComplete="current-password"
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button size={"sm"} variant={"link"} asChild>
+                <Link href="/auth/reset">Forgot your password</Link>
+              </Button>
+            </div>
+            <Button
+              type="submit"
+              className={cn(
+                "w-full my-4",
+                status === "executing" ? "animate-pulse" : ""
+              )}
+            >
+              Register
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </AuthCard>
+  );
+};
