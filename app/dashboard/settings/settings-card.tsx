e275df0e5202335eeb783fe24ddb761786e4dc88
Antonov-S <anto1nov1@gmail.com>
2024-07-15 03:14:02 +0300

    Setting up UploadThing

--- app/dashboard/settings/settings-card.tsx
+++ app/dashboard/settings/settings-card.tsx
@@ -31,13 +31,14 @@ import { Switch } from "@/components/ui/switch";
 import { FormError } from "@/components/auth/form-error";
 import { FormSuccess } from "@/components/auth/form-success";
 import { settings } from "@/server/actions/settings";
+import { UploadButton } from "@/app/api/uploadthing/upload";
 
 type SettingsCardProps = {
   session: Session;
 };
 
 export default function SettingsCard(session: SettingsCardProps) {
-  console.log(session);
+  // console.log(session);
 
   const form = useForm<z.infer<typeof SettingsSchema>>({
     resolver: zodResolver(SettingsSchema),
@@ -119,6 +120,32 @@ export default function SettingsCard(session: SettingsCardProps) {
                         alt="User Image"
                       />
                     )}
+                    <UploadButton
+                      className="scale-75 ut-button:ring-primary  ut-label:bg-red-50  ut-button:bg-primary/75  hover:ut-button:bg-primary/100 ut:button:transition-all ut-button:duration-500  ut-label:hidden ut-allowed-content:hidden"
+                      endpoint="avatarUploader"
+                      onUploadBegin={() => {
+                        setAvatarUploading(true);
+                      }}
+                      onUploadError={error => {
+                        form.setError("image", {
+                          type: "validate",
+                          message: error.message
+                        });
+                        setAvatarUploading(false);
+                        return;
+                      }}
+                      onClientUploadComplete={res => {
+                        form.setValue("image", res[0].url!);
+                        setAvatarUploading(false);
+                        return;
+                      }}
+                      content={{
+                        button({ ready }) {
+                          if (ready) return <div>Change Avatar</div>;
+                          return <div>Uploading...</div>;
+                        }
+                      }}
+                    />
                   </div>
                   <FormControl>
                     <Input
