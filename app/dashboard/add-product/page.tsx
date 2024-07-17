1664b4b05278e604a0fe4795d31fcbe332c6f561
Antonov-S <anto1nov1@gmail.com>
2024-07-18 00:09:50 +0300

    Removing form declaration from products page component(small bug)

--- app/dashboard/add-product/page.tsx
+++ app/dashboard/add-product/page.tsx
@@ -1,22 +1,10 @@
 import { auth } from "@/server/auth";
-import { ProductSchema, zProductSchema } from "@/types/product-schema";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { redirect } from "next/navigation";
-import { useForm } from "react-hook-form";
+
 import ProductForm from "./product-form";
 
 export default async function AddProduct() {
   const session = await auth();
-
-  const form = useForm<zProductSchema>({
-    resolver: zodResolver(ProductSchema),
-    defaultValues: {
-      title: "",
-      description: "",
-      price: 0
-    }
-  });
-
   if (session?.user.role !== "admin") return redirect("/dashboard/settings");
 
   return <ProductForm />;
